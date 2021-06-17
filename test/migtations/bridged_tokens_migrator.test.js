9b20be6510281786f0b035863bb364ef739808bf
mul53 <mulengabowa53@gmail.com>
2021-06-17 13:15:52 +0200

    add more tests

--- test/migtations/bridged_tokens_migrator.test.js
+++ test/migtations/bridged_tokens_migrator.test.js
@@ -4,6 +4,7 @@ const BridgedTokensMigrator = artifacts.require('BridgedTokensMigrator.sol')
 const PrimaryHomeMultiAMBErc20ToErc677 = artifacts.require('PrimaryHomeMultiAMBErc20ToErc677.sol')
 const ForeignMultiAMBErc20ToErc677 = artifacts.require('ForeignMultiAMBErc20ToErc677.sol')
 const AMBMock = artifacts.require('AMBMock.sol')
+const BridgeMock = artifacts.require('BridgeMock.sol')
 const { expect } = require('chai')
 const { getEvents, expectEventInLogs, ether, strip0x } = require('../helpers/helpers')
 const { ZERO_ADDRESS, toBN } = require('../setup')
@@ -19,7 +20,7 @@ const minPerTx = ether('0.01')
 const executionDailyLimit = dailyLimit
 const executionMaxPerTx = maxPerTx
 
-contract('BridgedTokensMigrator', async accounts => {
+contract.only('BridgedTokensMigrator', async accounts => {
   const owner = accounts[0]
   const user = accounts[1]
 
@@ -83,27 +84,28 @@ contract('BridgedTokensMigrator', async accounts => {
           await contract.upgradeToken(deprecatedToken.address, bridge.address, { from: user }).should.be.rejected
         })
 
-        it('upgrading token from owner should pass', async () => {
+        it('upgrading token from owner should deploy new upgraded token', async () => {
           const { logs } = await contract.upgradeToken(deprecatedToken.address, bridge.address, { from: owner }).should
             .be.fulfilled
           const events = await getEvents(contract, { event: 'TokenUpgraded' })
-          const deprecated = events[0].returnValues.deprecatedToken
-          const upgradedToken = events[0].returnValues.upgradedToken
+          const deprecatedTokenAddress = events[0].returnValues.deprecatedToken
+          const upgradedTokenAddress = events[0].returnValues.upgradedToken
 
           expectEventInLogs(logs, 'TokenUpgraded')
           expect(events.length).to.be.equal(1)
+          expect(deprecatedTokenAddress).to.be.equal(deprecatedToken.address)
 
-          expect(deprecated).to.be.equal(deprecatedToken.address)
-          // TODO: test for upgraded token
-          // expect(upgradedToken).to.be.equal(token.address)
+          expect(await contract.upgradedTokenAddress(deprecatedToken.address)).to.be.equal(upgradedTokenAddress)
+          expect(await contract.deprecatedTokenAddress(upgradedTokenAddress)).to.be.equal(deprecatedToken.address)
 
-          expect(await contract.upgradedTokenAddress(deprecated)).to.be.equal(upgradedToken)
-          expect(await contract.deprecatedTokenAddress(upgradedToken)).to.be.equal(deprecated)
+          const upgradedToken = await ERC677MultiBridgeMintableToken.at(upgradedTokenAddress)
+          expect(await upgradedToken.name()).to.be.equal(await deprecatedToken.name())
+          expect(await upgradedToken.symbol()).to.be.equal(await deprecatedToken.symbol())
+          expect(await upgradedToken.decimals()).to.be.bignumber.equal(await deprecatedToken.decimals())
         })
 
         it('migrate tokens should be rejected if tokens are not registered', async () => {
           await deprecatedToken.mint(user, oneEther).should.be.fulfilled
-          await token.addBridge(contract.address).should.be.fulfilled
           await deprecatedToken.approve(contract.address, oneEther, { from: user }).should.be.fulfilled
 
           await contract.migrateTokens(deprecatedToken.address, oneEther, { from: user }).should.be.rejected
@@ -136,9 +138,6 @@ contract('BridgedTokensMigrator', async accounts => {
             await contract.upgradeToken(upgradedToken.address, bridge.address, { from: owner }).should.be.rejected
           })
 
-          // TODO: add tests #addBridgePerToken
-          // TODO: add tests for #removeBridgePerToken
-
           describe('migrateTokens', async () => {
             it('migrate tokens should pass', async () => {
               await deprecatedToken.mint(user, oneEther).should.be.fulfilled
@@ -156,6 +155,78 @@ contract('BridgedTokensMigrator', async accounts => {
               await deprecatedToken.approve(contract.address, oneEther, { from: user }).should.be.fulfilled
 
               await contract.migrateTokens(deprecatedToken.address, oneEther, { from: user }).should.be.rejected
+            })
+          })
+
+          describe('addBridgePerToken', () => {
+            let secondaryBridge
+
+            beforeEach(async () => {
+              secondaryBridge = await BridgeMock.new(upgradedToken.address)
+            })
+
+            it('adding bridge from not owner should be rejected', async () => {
+              await contract.addBridgePerToken(secondaryBridge.address, upgradedToken.address, { from: user }).should.be
+                .rejected
+              expect(await upgradedToken.isBridge(secondaryBridge.address)).to.be.equal(false)
+              expect(await upgradedToken.bridgeCount()).to.be.bignumber.equal('0')
+              await secondaryBridge.mint(user, oneEther).should.be.rejected
+            })
+
+            it('adding bridge from an owner should fulfilled', async () => {
+              await contract.addBridgePerToken(secondaryBridge.address, upgradedToken.address, { from: owner })
+              expect(await upgradedToken.isBridge(secondaryBridge.address)).to.be.equal(true)
+              expect(await upgradedToken.bridgeCount()).to.be.bignumber.equal('1')
+              await secondaryBridge.mint(user, oneEther).should.be.fulfilled
+            })
+
+            it('adding multiple bridges from an owner should fulfilled', async () => {
+              const thirdBridge = await BridgeMock.new(upgradedToken.address)
+              await contract.addBridgePerToken(secondaryBridge.address, upgradedToken.address, { from: owner }).should
+                .be.fulfilled
+              await contract.addBridgePerToken(thirdBridge.address, upgradedToken.address, { from: owner }).should.be
+                .fulfilled
+              expect(await upgradedToken.bridgeCount()).to.be.bignumber.equal('2')
+              await secondaryBridge.mint(user, oneEther).should.be.fulfilled
+              await thirdBridge.mint(user, oneEther).should.be.fulfilled
+            })
+
+            it('adding same bridge multiple times should be rejected', async () => {
+              await contract.addBridgePerToken(secondaryBridge.address, upgradedToken.address, { from: owner }).should
+                .be.fulfilled
+              await contract.addBridgePerToken(secondaryBridge.address, upgradedToken.address, { from: owner }).should
+                .be.rejected
+              expect(await upgradedToken.bridgeCount()).to.be.bignumber.equal('1')
+            })
+          })
+
+          describe('removeBridgePerToken', () => {
+            beforeEach(async () => {
+              await contract.addBridgePerToken(contract.address, upgradedToken.address, { from: owner }).should.be
+                .fulfilled
+            })
+
+            it('removing bridge from not an owner should be rejected', async () => {
+              await contract.removeBridgePerToken(contract.address, upgradedToken.address, { from: user }).should.be
+                .rejected
+              expect(await upgradedToken.isBridge(contract.address)).to.be.equal(true)
+              expect(await upgradedToken.bridgeCount()).to.be.bignumber.equal('1')
+            })
+
+            it('removing bridge from an owner should be fulfilled', async () => {
+              await contract.removeBridgePerToken(contract.address, upgradedToken.address, { from: owner }).should.be
+                .fulfilled
+              expect(await upgradedToken.isBridge(contract.address)).to.be.equal(false)
+              expect(await upgradedToken.bridgeCount()).to.be.bignumber.equal('0')
+            })
+
+            it('removing bridge twice from an owner should be rejected', async () => {
+              await contract.removeBridgePerToken(contract.address, upgradedToken.address, { from: owner }).should.be
+                .fulfilled
+              expect(await upgradedToken.isBridge(contract.address)).to.be.equal(false)
+              expect(await upgradedToken.bridgeCount()).to.be.bignumber.equal('0')
+              await contract.removeBridgePerToken(contract.address, upgradedToken.address, { from: owner }).should.be
+                .rejected
             })
           })
         })
